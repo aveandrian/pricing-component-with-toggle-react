8abaa35de15d0c9cbc871e8749f39f4f4cdc5974
aveandrian <trancemilly7@gmail.com>
2023-06-06 22:01:10 +0200

    added elements and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import pricing from './pricing.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAnnual, setIsAnnual] = useState(false)
 
+  function toggleSwitch(){
+    setIsAnnual(prev => !prev)
+  }
   return (
-    <>
-      Our Pricing
-  Annually
-  Monthly
-
-  Basic
-  &dollar;19.99
-  &dollar;199.99
-  500 GB Storage
-  2 Users Allowed
-  Send up to 3 GB
-  Learn More
-
-  Professional
-  &dollar;24.99
-  &dollar;249.99
-  1 TB Storage
-  5 Users Allowed
-  Send up to 10 GB
-  Learn More
-
-  Master
-  &dollar;39.99
-  &dollar;399.99
-  2 TB Storage
-  10 Users Allowed
-  Send up to 20 GB
-  Learn More
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </div>
-    </>
+    <main>
+      <h1 className='title'>Our Pricing</h1>
+      <div className='switch-container'>
+        <p>Anually</p>
+        <div className='switch' onClick={toggleSwitch}>
+          <div className={`switch-circle ${isAnnual ? "annual" : ""}`}></div>
+        </div>
+        <p>Monthly</p>
+      </div>
+      <div className='pricing-wrapper'>
+        <div className="pricing-container left">
+            <p className="pricing-name">{pricing.basic.name}</p>
+            <h1 className="pricing-price">${isAnnual ? pricing.basic.anually :pricing.basic.monthly}</h1>
+            <div className="pricing-types">
+                <div className="divider"></div>
+                <p className="pricing-storage">{pricing.basic.storage} Storage</p>
+                <div className="divider"></div>
+                <p className="pricing-users">{pricing.basic.users} Users Allowed</p>
+                <div className="divider"></div>
+                <p className="pricing-send">Send up to {pricing.basic.send}</p>
+                <div className="divider"></div>
+            </div>
+            <button className="pricing-btn">Learn More</button>
+        </div>
+        <div className="pricing-container center">
+            <p className="pricing-name">{pricing.pro.name}</p>
+            <h1 className="pricing-price">${isAnnual ? pricing.pro.anually : pricing.pro.monthly}</h1>
+            <div className="pricing-types">
+                <div className="divider"></div>
+                <p className="pricing-storage">{pricing.pro.storage} Storage</p>
+                <div className="divider"></div>
+                <p className="pricing-users">{pricing.pro.users} Users Allowed</p>
+                <div className="divider"></div>
+                <p className="pricing-send">Send up to {pricing.pro.send}</p>
+                <div className="divider"></div>
+            </div>
+            <button className="pricing-btn">Learn More</button>
+        </div>
+        <div className="pricing-container right">
+            <p className="pricing-name">{pricing.master.name}</p>
+            <h1 className="pricing-price">${isAnnual ? pricing.master.anually :pricing.master.monthly}</h1>
+            <div className="pricing-types">
+                <div className="divider"></div>
+                <p className="pricing-storage">{pricing.master.storage} Storage</p>
+                <div className="divider"></div>
+                <p className="pricing-users">{pricing.master.users} Users Allowed</p>
+                <div className="divider"></div>
+                <p className="pricing-send">Send up to {pricing.master.send}</p>
+                <div className="divider"></div>
+            </div>
+            <button className="pricing-btn">Learn More</button>
+        </div>
+      </div>
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/aveandrian">aveandrian</a>.
+      </div>
+    </main>
   )
 }
 
